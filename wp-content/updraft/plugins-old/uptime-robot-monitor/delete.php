6dc804f00d66e1189cef54dafc6098bc494a0dfe
DBalivo-create <domenicobalivo@gmail.com>
2025-01-02 16:12:34 -0500

    italianpot

--- wp-content/updraft/plugins-old/uptime-robot-monitor/delete.php
+++
@@ -1,7 +0,0 @@
-<?php defined('ABSPATH') or die("No script kiddies please!");
-
-	global $wpdb;
-	$charset_collate = $wpdb->get_charset_collate();
-	$table_name = $wpdb->base_prefix . 'urpro';
- 	$sql = "DROP TABLE IF EXISTS $table_name";
-        $wpdb->query($sql);
